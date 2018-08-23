f44d0f27f2d1480574c4f07322b85378c3bea6af
Till Kottmann <deletescape@gmail.com>
2018-08-23 20:15:22 +0200

    Fix support for password resetting

--- app/src/main/java/deletescape/ch/sportdb/SportDBWebViewClient.java
+++ app/src/main/java/deletescape/ch/sportdb/SportDBWebViewClient.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class SportDBWebViewClient extends WebViewClient {
     private OnTitleListener onTitleListener;
@@ -13,7 +14,10 @@ public class SportDBWebViewClient extends WebViewClient {
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
         super.onPageStarted(view, url, favicon);
-        if (!url.startsWith("https://www.sportdb.ch/extranet/start.do") && !url.startsWith("https://www.sportdb.ch/extranet/mobile/mobileAwk.do") && !url.startsWith("https://www.sportdb.ch/extranet/j_security_check") && !url.startsWith("https://www.sportdb.ch/extranet/benutzer/benutzerStart.do?ButtonLogout=true")) {
+        if (!url.startsWith("https://www.sportdb.ch/extranet/start.do") && !url.startsWith("https://www.sportdb.ch/extranet/mobile/mobileAwk.do") && !url.startsWith("https://www.sportdb.ch/extranet/j_security_check") && !url.startsWith("https://www.sportdb.ch/extranet/benutzer/benutzerStart.do?ButtonLogout=true") && !url.startsWith("https://www.sportdb.ch/extranet/public/passwordLost.do?")) {
+            if("https://www.sportdb.ch/extranet/public/passwordLost.do".equals(url)){
+                Toast.makeText(view.getContext(), R.string.password_reset_success ,Toast.LENGTH_LONG).show();
+            }
             view.loadUrl("https://www.sportdb.ch/extranet/mobile/mobileAwk.do");
         }
     }
